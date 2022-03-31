0001784e09ba041ba3ad75d47d9cf2797948b4c8
Charley Reeson <neon.fortytwo@gmail.com>
2022-03-31 20:28:16 +0100

    feature addition: ghost breeds.

--- src/frontend/src/app/globals.js
+++ src/frontend/src/app/globals.js
@@ -1,9 +1,21 @@
 import breedtable from "@/breed-definitions.json";
 import utils from "@/app/utils";
 
+// add some default metadata
+breedtable.forEach((breed, index) => {
+    breedtable[index].metaData.ghost = false;
+});
+
 // different breed tables for different needs
 let GLOBALS ={
-    placeholder_breed: {name: "Placeholder", image: "unknown"},
+    placeholder_breed: {
+        name: "Placeholder",
+        image: "unknown",
+        metaData:{
+            category: "dragon",
+            ghost: false
+        }
+    },
     breeds: {
         entire: breedtable,
         males: utils.filterBreedTableByGender(breedtable, 'm'),
@@ -12,6 +24,6 @@ let GLOBALS ={
 }
 
 // Prevent modification
-Object.freeze(GLOBALS);
+//Object.freeze(GLOBALS);
 
 export default GLOBALS;
