d0ff4230b59a08c647b19a6ba0e16ed38a6021e2
Charley Reeson <neon.fortytwo@gmail.com>
2022-04-09 22:41:30 +0100

    Header menu moved to own component and mobile improved.

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,37 +1,8 @@
 <template>
   <div id="app" :class='skin'>
-    <div id='top'>
-      <header id='header' class='central-block'>
-        <div id='header-title'>
-          <router-link to="/" id="logo"><h1>Lineage Builder</h1></router-link>
-          <div class='part-of'>Part of <a href='/dc/tools'>Chazza's DC Tools</a></div>
-          <div class='subtitle'>v{{appVersion}} &copy; eden chazard</div>
-        </div>
-        <div id='header-right'>
-          <nav id="menu">
-            <ul>
-              <li><router-link to="/">Build</router-link></li>
-              <li><router-link to="/checkers">Checker Generator</router-link></li>
-              <li><router-link to="/ghost-breeds">Ghost Breeds</router-link></li>
-              <li><router-link to="/how-to">How To Use</router-link></li>
-              <li><router-link to="/disclaimer">Disclaimer</router-link></li>
-              <li><a href="https://dragcave.net/">DragCave</a></li>
-            </ul>
-          </nav>
-          Skin: 
-          <select
-            title="Skin"
-            v-model="skin"
-            @change="setSkin(skin)">
-            <option
-              v-for="skin in availableSkins"
-              :value="skin.cssName"
-              :key='skin.cssName'>{{skin.prettyName}}
-            </option>
-          </select>
-        </div>
-      </header>
-    </div>
+    <Header
+      :skin="skin"
+      @skinChanged="updateSkin"/>
     <main id='content'>
       <router-view :key="$route.fullPath"></router-view>
     </main>
@@ -41,39 +12,21 @@
 <script>
 import 'reset-css';
 import '@/assets/theming.css';
-import * as ls from "local-storage";
-
+import Header from '@/components/Header';
 export default {
   name: 'App',
-  components: {  },
+  components: { Header },
 
   data() {
     return {
-      appVersion: process.env.VUE_APP_VERSION,
       skin: '',
-      availableSkins: [
-        { cssName: 'skin-default', prettyName: 'Default' },
-        { cssName: 'skin-portal2', prettyName: 'Portal 2' },
-        { cssName: 'skin-portal2-light', prettyName: 'Portal 2 Light' },
-        { cssName: "skin-mobile-dark", prettyName: "Mobile/Tablet Dark" }
-      ]
     }
   },
-  mounted(){
-    // set default skin if no skin set
-    this.setSkin(ls.get('skin') || 'skin-default');
-  },
-  methods:{
-    setSkin(to){
-      // ensure valid skin in case it's been edited by user
-      // in localstorage
-      if(!this.availableSkins.find((skin) => skin.cssName === to)){
-        this.setSkin('skin-default');
-        return;
-      }
 
-      ls.set('skin', to);
-      this.skin = to;
+  methods:{
+    updateSkin(newSkin){
+      this.skin = newSkin;
+      console.log(newSkin)
     }
   }
 }
@@ -87,64 +40,21 @@ export default {
     background:var(--builderBG);
     min-height: 100vh;
 }
-#top{
-    color:#fff;
-    background: var(--headerBG);
-    margin-bottom: 5px;
-}
-#header{
-    padding: 10px;
-    display: flex;
-    flex-direction: column;
-}
-#header-title{
-    text-align: center;
-}
-#header .part-of a{
-  color:#fff;
-}
-#header-right{
-    text-align: center;
-}
-#logo{
-    text-decoration: none;
-    color:#fff;
-    display: inline-block;
-}
-#menu{
-    margin:5px 0px;
-}
-#menu ul{
-    display:flex;
-    flex-direction: row;
-    justify-content: center;
-}
-#menu a{
-    color:#fff;
-    font-size: 15px;
-    text-decoration: none;
-}
-#menu li:after{
-    color: #fff;
-    content: "\00B7";
-    margin: 0 4px;
-}
-#menu li:last-child:after {
-    content: ''; 
-}
+
 #content{
-    padding:5px;
 }
 
 /* global css */
 :root {
     --maxwidth: 1000px;
+    --desktop: 1024px;
+    --mobile: 768px;
 }
 html{
     height: 100%;
     /* fixes bug with the layout not stretching for big lineages */
-    min-width: -webkit-fill-available;
-    min-width: fit-content;
+    overflow-x:hidden;
+    width:100vw;
 }
 body{
     font: 14px/19.6px "PT Serif", "Times New Roman", "Times", serif;
@@ -173,6 +83,7 @@ h4 {
 .central-block{
     max-width: var(--maxwidth);
     margin:0px auto;
+    padding:0px 5px;
 }
 .list{
     list-style-type: disc;
@@ -208,25 +119,5 @@ in LineageBuilder.vue, that's why these are !importanted.*/
 }
 .modal .btn-close{
     color:var(--modalHeaderFG) !important;
-}
-
-@media only screen and (min-width: 768px){
-  #top{
-        height: 165px;
-  }
-  #header{
-        flex-direction: row;
-  }
-  #menu ul{
-        justify-content: flex-end;
-  }
-  #header-right{
-        flex:1;
-        margin-top: 17px;
-        text-align: right;
-  }
-  #header-title{
-        text-align: left;
-  }
 }
 </style>
---
+++ src/frontend/src/components/Header.vue
@@ -0,0 +1,148 @@
+<template>
+<div id='top'>
+    <header id='header' class='central-block'>
+        <div id='header-title'>
+            <router-link to="/" id="logo"><h1>Lineage Builder</h1></router-link>
+            <div class='part-of'>Part of <a href='/dc/tools'>Chazza's DC Tools</a></div>
+            <div class='subtitle'>v{{appVersion}} &copy; eden chazard</div>
+        </div>
+        <div id='header-right'>
+            <nav id="menu">
+            <ul>
+                <li><router-link to="/">Build</router-link></li>
+                <li><router-link to="/checkers">Checker Generator</router-link></li>
+                <li><router-link to="/ghost-breeds">Ghost Breeds</router-link></li>
+                <li><router-link to="/how-to">How To Use</router-link></li>
+                <li><router-link to="/disclaimer">Disclaimer</router-link></li>
+                <li><a href="https://dragcave.net/">DragCave</a></li>
+            </ul>
+            </nav>
+            <div>
+            Skin: 
+            <select
+                title="Skin"
+                :value="skin"
+                @change="setSkin($event.target.value)">
+                <option
+                  v-for="skin in availableSkins"
+                  :value="skin.cssName"
+                  :key='skin.cssName'>{{skin.prettyName}}
+                </option>
+            </select>
+            </div>
+        </div>
+    </header>
+</div>
+</template>
+
+<script>
+import * as ls from "local-storage";
+
+export default {
+    props: {
+        skin: String,
+    },
+  data() {
+    return {
+      appVersion: process.env.VUE_APP_VERSION,
+      availableSkins: [
+        { cssName: 'skin-default', prettyName: 'Default' },
+        { cssName: 'skin-portal2', prettyName: 'Portal 2' },
+        { cssName: 'skin-portal2-light', prettyName: 'Portal 2 Light' },
+        { cssName: "skin-mobile-dark", prettyName: "Mobile/Tablet Dark" }
+      ]
+    }
+  },
+  
+  mounted(){
+    // set default skin if no skin set
+    this.setSkin(ls.get('skin') || 'skin-default');
+  },
+
+  methods:{
+    setSkin(to){
+      console.log(to)
+      // ensure valid skin in case it's been edited by user
+      // in localstorage
+      if(!this.availableSkins.find((skin) => skin.cssName === to)){
+        this.setSkin('skin-default');
+        return;
+      }
+
+      ls.set('skin', to);
+      this.$emit('skinChanged', to);
+    }
+  }
+}
+</script>
+<style scoped>
+#top{
+    color:#fff;
+    background: var(--headerBG);
+    margin-bottom: 5px;
+}
+#header{
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+}
+#header-title{
+    text-align: center;
+}
+#header .part-of a{
+  color:#fff;
+}
+#header-right{
+    text-align: center;
+}
+#logo{
+    text-decoration: none;
+    color:#fff;
+    display: inline-block;
+}
+#menu{
+    margin:5px 0px;
+}
+#menu ul {
+  display:grid;
+}
+#menu ul{
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: auto;
+}
+#menu a{
+    color:#fff;
+    font-size: 15px;
+    text-decoration: none;
+}
+
+@media only screen and (min-width: 1024px){
+  #top{
+        height: 165px;
+  }
+  #header{
+        flex-direction: row;
+  }
+  #menu ul{
+      display: flex;
+      justify-content: flex-end;
+  }
+  #menu li:after{
+      color: #fff;
+      content: "\00B7";
+      margin: 0 4px;
+  }
+  #menu li:last-child:after {
+      content: ''; 
+  }
+  #header-right{
+        flex:1;
+        margin-top: 17px;
+        text-align: right;
+  }
+  #header-title{
+        text-align: left;
+  }
+}
+</style>
