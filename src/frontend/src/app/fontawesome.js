d144a67068c3762b7375a6efa9853869f8e510f3
Charley Reeson <neon.fortytwo@gmail.com>
2022-05-02 09:38:37 +0100

    Delete children changed to a pair of scissors.

--- src/frontend/src/app/fontawesome.js
+++ src/frontend/src/app/fontawesome.js
@@ -1,11 +1,11 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {  faMinus, faVenusMars, faSave, faFileCode, faArrowLeft, faArrowRight,
-          faSyncAlt, faArrowUp, faTimes, faClone, faPaste, faSearch, faFont, faExclamationTriangle,
-        faLink, faCopy} from '@fortawesome/free-solid-svg-icons';
+          faSyncAlt, faArrowUp, faClone, faPaste, faSearch, faFont, faExclamationTriangle,
+        faLink, faCopy, faCut} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 library.add(faMinus, faVenusMars, faSave, faFileCode, faArrowLeft, faArrowRight,
-            faClone, faPaste, faSyncAlt, faArrowUp, faTimes, faSearch, faFont, faExclamationTriangle,
-            faLink, faCopy);
+            faClone, faPaste, faSyncAlt, faArrowUp, faSearch, faFont, faExclamationTriangle,
+            faLink, faCopy, faCut);
 
 export {
     library,
