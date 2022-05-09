0a3cc85bfb8f6827623ad9685cca2daf7985571f
Charley Reeson <neon.fortytwo@gmail.com>
2022-05-09 20:01:45 +0100

    oops

--- src/frontend/src/pages/PageView.vue
+++ src/frontend/src/pages/PageView.vue
@@ -5,9 +5,7 @@
             <h2 v-if='status.level == 2'>{{status.title}}</h2>
             <p>{{status.message}}</p>
         </div>
-      </div>
-      <div v-if="tree != null">
-        <div id='options'>
+        <div v-if="tree != null" id='options'>
           <span class='option'>
             <router-link :to="{ path: '/', query: { template: hash }}">Import into editor</router-link>
           </span>
@@ -15,6 +13,8 @@
             Share link: <TextCopy v-model="shareLink" type='input' />
           </span>
         </div>
+      </div>
+      <div v-if="tree != null">
         <Lineage
           :tree="tree"
           :config="config" />
