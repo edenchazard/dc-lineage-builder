279b04237f0c7b220fbf6ab80ec52673b8188e5e
Charley Reeson <neon.fortytwo@gmail.com>
2022-05-19 00:03:12 +0100

    changed dialogs

---
+++ src/frontend/src/components/Toolbar/DialogImport.vue
@@ -0,0 +1,72 @@
+<template>
+    <Dialog @close="$emit('close')">
+        <template v-slot:header>
+          Import lineage
+        </template>
+        <template v-slot:body>
+          <p>Paste the export text and click 'import'.</p>
+          <p>Please note if you have a lineage in progress, importing a new lineage will overwrite it.</p>
+          <div>
+            <TextCopy v-model="file" placeholder="Paste your import text here" />
+          </div>
+          <Information :info="status" />
+        </template>
+        <template v-slot:footer>
+          <button @click="importLineage">Import</button>
+        </template>
+    </Dialog>
+</template>
+<script>
+import Dialog from '@/components/Dialog';
+import { validators, utils } from '@/app/bundle.js';
+import Information from '@/components/ui/Information';
+import TextCopy from '@/components/ui/TextCopy';
+
+export default {
+  name: 'DialogImport',
+  components: { Dialog, Information, TextCopy },
+  props: {
+      show: Boolean,
+      tree: Object,
+      onImport: Function
+  },
+
+  data(){
+    return {
+      file: "",
+      status: {
+        level: 0,
+        message: ""
+      },
+    }
+  },
+
+  methods:{
+    importLineage(){
+      const errMsg = {
+        level: 3,
+        message: `Error reading export code. This export code could be corrupt or
+        include a ghost breed that hasn't been re-uploaded.`
+      };
+
+      try{
+        const importedTree = JSON.parse(this.file.trim());
+
+        if(!validators.verifyIntegrity(importedTree)){
+          this.status = errMsg;
+          return;
+        }
+
+        // add selection data
+        utils.forEveryDragon(importedTree, dragon => dragon.selected = false);
+
+        this.$emit('onImport', importedTree);
+        this.$emit('close');
+      }
+      catch{
+          this.status = errMsg;
+      }
+    }
+  }
+};
+</script>
