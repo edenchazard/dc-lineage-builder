40fbb324316f80ccaad834981ac399d7c594644d
Charley Reeson <neon.fortytwo@gmail.com>
2022-05-19 00:10:27 +0100

    prevent right click/long press and save image

--- src/frontend/src/components/DragonPortrait.vue
+++ src/frontend/src/components/DragonPortrait.vue
@@ -93,6 +93,15 @@ export default {
 .fallback{
     position: relative;
     image-rendering:-webkit-optimize-contrast;
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Old versions of Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none; /* Non-prefixed version, currently
+    supported by Chrome, Edge, Opera and Firefox */
+    -webkit-touch-callout:none;
+    pointer-events: none;
 }
 .ghost{
     margin:-1px -1px;
