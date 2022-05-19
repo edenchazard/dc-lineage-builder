9e16921515f5f86c2347ab08df770ae1e034f748
Charley Reeson <neon.fortytwo@gmail.com>
2022-05-20 00:09:44 +0100

    moved tag functionality to store.

---
+++ src/frontend/src/components/BreedTags.vue
@@ -0,0 +1,16 @@
+<template>
+<TagList
+    :value="$store.state.tags"
+    @updated="$store.commit('setTags', $event)" />
+</template>
+<script>
+import TagList from '@/components/ui/TagList';
+
+export default {
+    name: "BreedTags",
+
+    components: {
+        TagList
+    }
+}
+</script>
