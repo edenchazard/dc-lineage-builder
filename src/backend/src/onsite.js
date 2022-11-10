18cbe7890576d9093801a159e3b2c70c0eee7152
Charley Reeson <neon.fortytwo@gmail.com>
2022-11-10 18:14:41 +0000

    onsite preview progress

--- src/backend/src/onsite.js
+++ src/backend/src/onsite.js
@@ -1,67 +1,82 @@
 const axios = require("axios");
 const nodeHTMLParser = require('node-html-parser');
-//import util from 'util';
 
-const error = 'e';
-async function grabHTML(code){ //string){
-    try {
-        const response = await axios.get("https://dragcave.net/lineage/" + code);
-        const a = performance.now();
-        const root = nodeHTMLParser.parse(response.data);
-        const baseULTag = root.querySelector('._2y_j ul');
-
-        if(baseULTag.constructor.name === "HTMLElement"){
-            const innerHTML = baseULTag.toString();
-            const b = performance.now();
-            //console.log(util.inspect(div, {showHidden: false, depth: 2, colors: true}));
-            console.log(`parse complete in `+(b-a));
-            return innerHTML;
-        }
+class OnsiteError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'OnsiteError';
     }
-    catch (e){
-        error;
-    }
-
-    return null;
 }
 
-async function getRootHTMLForPair(codes){ //: [string, string]){
-    try{
-        const htmlGrabs = await Promise.all(codes.map(code => grabHTML(code)));
-
-        console.log(htmlGrabs)
-        // one lineage wasn't returned
-        if(htmlGrabs.includes(null))
-            error;
-
-        const [ male, female ] = htmlGrabs;
-
-        // create a root ul
-        const root = (
-            `<ul>
-                <li>
-                    <div>
-                        <img src="src/assets/placeholder.png" alt="result placeholder" />
-                        <label>Result</label>
-                    </div>
-                    <ul>
-                        <li>${male}</li>
-                        <li>${female}</li>
-                    </ul>
-                </li>
-            </ul>`)
-            // replace the shorthanded urls with full links to dragcave
-            .replaceAll('/images/', 'https://dragcave.net/images/')
-            .replaceAll('/view/', 'https://dragcave.net/view/');
-
-        return root;
+async function grabHTML(code){ //string){
+    const fetchDragon = async (code) => {
+        try{ 
+            return await axios.get(`https://dragcave.net/lineage/${code}`);
+        }
+        catch(err){
+            if(err.response){
+                if(err.response.status === 404)
+                    throw new OnsiteError(`Dragon not found. Possibly fogged or doesn't exist. Dragon: ${code}`);
+                else
+                    throw new OnsiteError(`Non-200 OK response when grabbing from DC. Dragon: ${code}`);
+            }
+            else
+                throw new OnsiteError(`Unknown error while retrieving. Dragon: ${code}`)
+        }
     }
-    catch (e){
-        error;
-    }
+
+    const response = await fetchDragon(code);
+    const a = performance.now();
+    const root = nodeHTMLParser.parse(response.data);
+
+    /* if(!root)
+        throw new Error("Error parsing dragon."); */
+
+    const baseULTag = root.querySelector('._2y_j ul');
+
+    if(baseULTag === null)
+        throw new OnsiteError(`Could not find ul tag. Dragon: ${code}`);
+    
+    const innerHTML = baseULTag.toString();
+    const b = performance.now();
+    console.log(`parse complete in ${(b-a)} for ${code}`);
+    return innerHTML;
+}
+
+async function createRootHTMLForPair(codes){ //: [string, string]){
+    const [ male, female ] = await Promise.all(codes.map(code => grabHTML(code)));
+
+    /* // one lineage wasn't returned
+    if(htmlGrabs.includes(null))
+        throw new OnsiteError("Missing dragon."); */
+
+    // create a root ul
+    const root = (
+        `<ul>
+            <li>
+                <div>
+                    <img src="src/assets/placeholder.png" alt="result placeholder" />
+                    <label>Result</label>
+                </div>
+                <ul>
+                    <li>${male}</li>
+                    <li>${female}</li>
+                </ul>
+            </li>
+        </ul>`)
+        // strip large whitespaces
+        .replace(/\s{2,}/g, '')
+        // add additional properties to links
+        .replaceAll("<a ", "<a rel='noopener noreferrer' target='_blank' ")
+        // replace the shorthanded urls with full links to dragcave
+        .replaceAll('/images/', 'https://dragcave.net/images/')
+        .replaceAll('/view/', 'https://dragcave.net/view/');
+
+    return root;
 }
 
 module.exports = {
+    OnsiteError,
     grabHTML,
-    getRootHTMLForPair
+    createRootHTMLForPair
 }
