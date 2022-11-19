ef14c6d5d9f0b82e31114b0bdede44d4f2354f24
Charley Reeson <neon.fortytwo@gmail.com>
2022-11-19 20:21:36 +0000

    improve modularity of code and usability of the feature.

--- src/frontend/src/components/GhostBreedUpload.vue
+++ src/frontend/src/components/GhostBreedUpload.vue
@@ -1,31 +1,44 @@
 <template>
-    <div class='breed-upload'>
-        <div v-if="dragon.image !== null">
-            <DragonPortrait
-                class="tile-portrait disabled"
-                :data="dragon" />
-        </div>
-        <input
-            type='file'
-            accept="image/png, image/gif"
-            @change="imageChanged" />
+<div class='breed-upload'>
+    <div
+        v-if="dragon.image !== null"
+        @click="fileInput?.click()">
+        <DragonPortrait
+            class="tile-portrait disabled"
+            :data="dragon" />
     </div>
-  </template>
-  <script setup lang="ts">
-  import { reactive } from "vue";
-  import GLOBALS from "../app/globals";
-  import settings from "../app/settings";
-  import { PortraitData } from "../app/types";
-  import DragonPortrait from "../components/DragonPortrait.vue";
-  
-  const emit = defineEmits<{
+    <input
+        :id="label"
+        ref="fileInput"
+        type='file'
+        accept="image/png, image/gif"
+        @change="imageChanged" />
+</div>
+</template>
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import GLOBALS from "../app/globals";
+import settings from "../app/settings";
+import { PortraitData } from "../app/types";
+import DragonPortrait from "../components/DragonPortrait.vue";
+
+defineProps({
+    label: {
+        type: String,
+        required: true
+    }
+});
+
+const emit = defineEmits<{
     (e: "tileChosen", base64Data: string): void,
     (e: "uploadError"): void
-  }>();
-  
-  // We have to use a "dummy" dragon to display the portrait
-  // unfortunately
-  const dragon = reactive<PortraitData>({
+}>();
+
+const fileInput = ref<HTMLInputElement>();
+
+// We have to use a "dummy" dragon to display the portrait
+// unfortunately
+const dragon = reactive<PortraitData>({
     // copy placeholder properties and replace what we need
     ...GLOBALS.placeholder,
     image: "/src/assets/placeholder.png",
@@ -34,17 +47,17 @@
         group: "Standard",
         tags: ["Regular"]
     }
-  });
-  
-  function imageChanged(e: Event){
+});
+
+function imageChanged(e: Event){
     const input = e.target as HTMLInputElement;
-  
+
     if (input.files instanceof FileList && input.files[0]) {
         const file = input.files[0];
-  
+
         if(file.size < settings.ghostBreedSize){
             const reader = new FileReader();
-  
+
             reader.addEventListener("load", () => {
                 if(typeof reader.result === "string"){
                     const data = reader.result;
@@ -53,10 +66,15 @@
                 }
                 else emit("uploadError");
             });
-  
+
             reader.readAsDataURL(file);
         }
         else emit("uploadError");
     }
-  }
-  </script>
+}
+</script>
+<style scoped>
+input[type='file']{
+    display:none;
+}
+</style>
