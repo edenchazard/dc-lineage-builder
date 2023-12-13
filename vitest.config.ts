03baa9e02de4276364fb2f4e0a8ae813e81bab73
Charley Reeson <neon.fortytwo@gmail.com>
2023-12-13 22:02:26 +0000

    fix tsconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
 
 export default mergeConfig(
-  viteConfig({ mode: 'dev', command: 'serve' }),
+  viteConfig({ mode: 'dev' }),
   defineConfig({
     test: {
       globals: true,
