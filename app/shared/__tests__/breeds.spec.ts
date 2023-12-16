2390af56d06fceabde61ccc1c46f759f6d2a77c4
Charley Reeson <neon.fortytwo@gmail.com>
2023-12-16 20:59:50 +0000

    add some tests for breeds.ts

---
+++ app/shared/__tests__/breeds.spec.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect } from 'vitest';
+import { listOfBreeds, malePortraits, placeholder } from '../breeds';
+
+describe('breeds.ts', () => {
+  it('exports a list of breeds with placeholder as first in the list', () => {
+    expect(Array.isArray(listOfBreeds)).to.be.true;
+    expect(listOfBreeds[0]).to.be.eql(placeholder);
+    expect(listOfBreeds.length).to.be.greaterThan(300);
+  });
+
+  it('exports a list of male portraits', () => {
+    expect(Array.isArray(malePortraits)).to.be.true;
+  });
+
+  it('exports a list of female portraits', () => {
+    expect(Array.isArray(malePortraits)).to.be.true;
+  });
+});
