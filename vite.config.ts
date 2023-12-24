142ca8d431abbd679faee05f2b9c23e316e1d8d7
Charley Reeson <neon.fortytwo@gmail.com>
2023-12-24 23:07:02 +0000

    drop plugin legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig, loadEnv } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 import { configDefaults } from 'vitest/config';
 
 export default ({ mode }: { mode: string }) => {
@@ -22,7 +21,7 @@ export default ({ mode }: { mode: string }) => {
         },
       },
     },
-    plugins: [vue(), legacy()],
+    plugins: [vue()],
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
