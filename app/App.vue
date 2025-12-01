dfda9e957bda35f550d7e711780d90cd9901cc94
edenchazard <38665888+edenchazard@users.noreply.github.com>
2025-12-01 19:12:05 +0000

    fix: rogue space in commit reference (#129)

--- app/App.vue
+++ app/App.vue
@@ -41,16 +41,14 @@
           :href="`https://github.com/edenchazard/dc-lineage-builder/releases/tag/v${appStore.appVersion}`"
           target="_blank"
           rel="noopener noreferrer"
+          >v{{ appStore.appVersion }}</a
         >
-          v{{ appStore.appVersion }}
-        </a>
         <span v-if="appStore.appCommitSha !== 'unknown'">
           (<a
             :href="`https://github.com/edenchazard/dc-lineage-builder/commit/${appStore.appCommitSha}`"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            #{{ appStore.appCommitSha.substring(0, 6) }} </a
+            >#{{ appStore.appCommitSha.substring(0, 6) }}</a
           >)
         </span>
         <div>eden chazard</div>
